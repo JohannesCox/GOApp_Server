ad60e56ee3f34c51a7fedd96e51c36abd5b92d41
Martin <Martin@141.3.214.182>
2017-08-04 13:38:02 +0200

    Class User and hibernate.cfg.xml added

---
+++ src/main/java/Database/User.java
@@ -0,0 +1,42 @@
+package database;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Table;
+
+@Entity
+@Table(appliesTo = "User")
+class User {
+	@Id @GeneratedValue
+	@Column(name="userID")
+private String userID;
+	@Column(name="username")
+private String username;
+	public User(String userID, String username) {
+		this.userID = userID;
+		this.username = username;
+	}
+	public String getUserID() {
+	return userID;
+}
+
+public void setUserID(String userID) {
+	this.userID = userID;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+	public User() {
+		
+	}
+
+}
