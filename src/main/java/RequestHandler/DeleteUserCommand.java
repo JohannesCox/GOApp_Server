ff1a517989c24701d88fc93b99f3a34a1a04525b
Johannes Cox <jocox@hotmail.de>
2017-08-04 18:11:52 +0200

    Updated pom.xml and web.xml

--- src/main/java/RequestHandler/DeleteUserCommand.java
+++ src/main/java/RequestHandler/DeleteUserCommand.java
@@ -1,5 +1,10 @@
 package RequestHandler;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import Database.UserHandler;
+
 public class DeleteUserCommand extends Command {
 	
 	private String userId;
@@ -9,7 +14,16 @@ public class DeleteUserCommand extends Command {
 	}
 	
 	public String process() {
-		//TODO
-		return null;
+		UserHandler uh = new UserHandler();
+		boolean succesfull = uh.deleteUser(userId);
+		
+		JsonObject jsonObj = new JsonObject();
+		
+		jsonObj.addProperty("successfull", succesfull);
+		
+		JsonArray ja = new JsonArray();
+		ja.add(jsonObj);
+		return ja.toString();
+		
 	}
 }
