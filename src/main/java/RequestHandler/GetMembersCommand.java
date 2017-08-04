a4ddd056c096e8843827725b7ae8332fe39c3dc6
Johannes Cox <jocox@hotmail.de>
2017-08-04 15:56:43 +0200

    Restlichen RequestHandler hinzugefügt. Process() Mehtoden müssen jeweils noch implementiert werden.

---
+++ src/main/java/RequestHandler/GetMembersCommand.java
@@ -0,0 +1,11 @@
+package RequestHandler;
+
+/**
+ *Returns a List of all members of an event. The username and if the user is an admin
+ *of the event is returned.
+ */
+public class GetMembersCommand {
+	
+	private String userId;
+	private String eventId;
+}
