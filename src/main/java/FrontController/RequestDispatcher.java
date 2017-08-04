1d884f4c503ba1912ca636678a89c7d1d5ec4c9f
Johannes Cox <jocox@hotmail.de>
2017-08-04 19:56:10 +0200

    Updated Request Dispatcher

---
+++ src/main/java/FrontController/RequestDispatcher.java
@@ -0,0 +1,18 @@
+package FrontController;
+
+import javax.servlet.http.HttpServletRequest;
+
+import RequestHandler.Command;
+
+public class RequestDispatcher {
+	
+	public RequestDispatcher(HttpServletRequest request) {
+		//TODO
+	}
+	
+	public Command createHandler() {
+		//TODO
+		return null;
+	}
+
+}
