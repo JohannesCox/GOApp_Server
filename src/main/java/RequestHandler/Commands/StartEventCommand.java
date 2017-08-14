2ebffbb0c99a7f98d3f342f7d3ae7af715e5d117
Johannes Cox <jocox@hotmail.de>
2017-08-14 16:45:07 +0200

    Created dummy points for testing

--- src/main/java/RequestHandler/Commands/StartEventCommand.java
+++ src/main/java/RequestHandler/Commands/StartEventCommand.java
@@ -21,6 +21,36 @@ public class StartEventCommand extends Command {
 		userId = uId;
 		eventId = eId;
 		doublepoint = dp;
+		
+		//TODO Delete! For Testing only
+		
+		//Start creating dummy points
+		if(!algorithms.containsKey("TestEvent")) {
+			ClusteringAlgorithm ca = new ClusteringAlgorithm();
+			String user1 = "user1";
+			double[] gps1 = {49.009428, 8.404064};
+			String user2 = "user2";
+			double[] gps2 = {49.009428,8.404257}; //user1 and user2 are in one group
+			String user3 = "user3";
+			double[] gps3 = {49.011729,8.404064};
+			String user4 = "user4";
+			double[] gps4 = {49.011680, 8.403946}; //user3 and user4 are in one group
+			String user5 = "user5";
+			double[] gps5 = {49.011603, 8.397154}; //user5 is alone
+			DoublePoint loc1 = new DoublePoint(gps1);
+			DoublePoint loc2 = new DoublePoint(gps2);
+			DoublePoint loc3 = new DoublePoint(gps3);
+			DoublePoint loc4 = new DoublePoint(gps4);
+			DoublePoint loc5 = new DoublePoint(gps5);
+			ca.updateGPS(user1, loc1);
+			ca.updateGPS(user2, loc2);
+			ca.updateGPS(user3, loc3);
+			ca.updateGPS(user4, loc4);
+			ca.updateGPS(user5, loc5);
+		}
+		//end creating dummy points
+		
+		
 	}
 	
 	public String process() {
